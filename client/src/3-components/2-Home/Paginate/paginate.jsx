71c88c977b97690993d39e622cad2025729cf3a3
joedurand-hub <53331000+joedurand-hub@users.noreply.github.com>
2021-08-01 21:09:06 -0300

    Finished

--- client/src/3-components/2-Home/Paginate/paginate.jsx
+++ client/src/3-components/2-Home/Paginate/paginate.jsx
@@ -4,7 +4,7 @@ import Card from '../../5-Card/GameCard'
 import './Paginate.css'
 // Tiene el paginado de las Cards
 
-export function Paginate({ videogame, title, dataLimit, load }) {
+export function Paginate({ videogame, title, dataLimit, pageLimit }) {
     const [pages] = useState(Math.round(videogame.length / dataLimit));
     const [currentPage, setCurrentPage] = useState(1);
 
@@ -13,6 +13,16 @@ export function Paginate({ videogame, title, dataLimit, load }) {
         const endIndex = startIndex + dataLimit
         return videogame.slice(startIndex, endIndex)
     };        
+
+    const getPaginationGroup = () => {
+        let start = Math.floor((currentPage - 1) / pageLimit) * pageLimit;
+        return new Array(pageLimit).fill().map((_, idx) => start + idx + 1);
+      };
+    
+    function changePage(event) {
+        const pageNumber = Number(event.target.textContent);
+        setCurrentPage(pageNumber);
+      }
 
     function goToNextPage() {
         setCurrentPage((pages) => pages + 1)
@@ -26,10 +36,10 @@ export function Paginate({ videogame, title, dataLimit, load }) {
         <div>
        <div className="containerPagination">
         <h1>{title}</h1>
-        <div className="button">
-            <button  onClick={goToPreviousPage}> Previous </button>    
-            <button  onClick={goToNextPage}> Next </button>
-        </div>    
+        <div className={`button ${videogame.length === 0 ? 'noExistence' : ''}`}>
+            <button className={`${currentPage === 1 ? 'disabled' : ''}`}  onClick={goToPreviousPage}> Previous </button>    
+            <button className={`${currentPage === 7 ? 'disabled' : ''}`} onClick={goToNextPage}> Next </button>
+        </div> 
         <div className="pagination">
           {getPaginatedData().map((game, id) => ( 
               <Card 
@@ -39,9 +49,14 @@ export function Paginate({ videogame, title, dataLimit, load }) {
               />
           ))}
         </div>
-
-    </div>
+        <div className={`${videogame.length === 0 ? 'noExistence' : 'buttonsPages'}`}>
+        {getPaginationGroup().map((item, index) => (
+            <button key={index} onClick={changePage} className={`paginationItem ${currentPage === item ? 'active' : null}`}
+            > <span>{item}</span> </button>
+      ))}
       </div>
+    </div>
+  </div>
   )
       
   
